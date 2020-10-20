372d83a0d01c90770c5f3d2910bb4f99f22169bb
vinod0354 <61411608+vinod0354@users.noreply.github.com>
2020-10-20 16:39:20 +0530

    recruit-bot

--- db/dbconfig.js
+++ db/dbconfig.js
@@ -1,20 +1,14 @@
 const mysql = require('mysql');
 
-// var dbConnection = mysql.createPool({
-//   host: 'localhost',
-//   port: 3306,
-//   user: 'root',
-//   password: 'root@123',
-//   database: 'a10mi6nt_recruit_bot'
-// });
-
 var dbConnection = mysql.createPool({
-  host: '111.118.215.187',
+  host: 'localhost',
   port: 3306,
-  user: 'a10mi6nt_recruit',
-  password: 'recruit@2020',
+  user: 'root',
+  password: 'root@123',
   database: 'a10mi6nt_recruit_bot'
 });
+
+
 
 dbConnection.getConnection(function(err) {
   if (err){ console.error('error connecting: ' + err);}
