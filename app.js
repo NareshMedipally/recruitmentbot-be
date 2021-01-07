3b9abe6d25a064cc18178bbf6679e2b54379d6be
Naresh <mnr@Nareshs-MacBook-Air.local>
2021-01-07 21:20:58 +0800

    deploy

--- app.js
+++ app.js
@@ -52,7 +52,7 @@ app.use('/api',update_enterprise);
 app.use('/api',email_auth);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}))
-app.use('/files', express.static(path.join(__dirname, '/uploads')));
+app.use('/api/files', express.static(path.join(__dirname, '/uploads')));
 
 
 app.use((req, res, next) => {
